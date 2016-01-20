640d9bd1e234516d2bfe7749e3f56a3802c646c4
talmeym <mtalmey@gmail.com>
2016-01-20 11:52:43 +0000

    moved isolate to be an attribute of sequence

--- src/test/java/com/emarte/regurgitator/test/ContainsJsonPathJsonLoaderTest.java
+++ src/test/java/com/emarte/regurgitator/test/ContainsJsonPathJsonLoaderTest.java
@@ -14,6 +14,6 @@ public class ContainsJsonPathJsonLoaderTest extends JsonLoaderTest {
 
 	@Test
 	public void testFullLoad() throws DocumentException, SAXException, IOException, RegurgitatorException {
-		assertExpectationFullLoad("classpath:/ContainsJsonPath_fullLoad.json", "com.emarte.regurgitator.core.Sequence:['regurgitator-configuration-1',[com.emarte.regurgitator.core.Decision:['decision-1',[com.emarte.regurgitator.test.stuff.TestStep:['test-step-1']],[com.emarte.regurgitator.core.Rule:['rule-1',[com.emarte.regurgitator.core.Condition:['condition-1',com.emarte.regurgitator.core.ContextLocation:['context:location'],'$.something.something',true,com.emarte.regurgitator.extensions.ContainsJsonPath:[]]],'test-step-1']],com.emarte.regurgitator.core.FirstMatchBehaviour:[],null]]]");
+		assertExpectationFullLoad("classpath:/ContainsJsonPath_fullLoad.json", "com.emarte.regurgitator.core.Sequence:['regurgitator-configuration-1',[com.emarte.regurgitator.core.Decision:['decision-1',[com.emarte.regurgitator.test.stuff.TestStep:['test-step-1']],[com.emarte.regurgitator.core.Rule:['rule-1',[com.emarte.regurgitator.core.Condition:['condition-1',com.emarte.regurgitator.core.ContextLocation:['context:location'],'$.something.something',true,com.emarte.regurgitator.extensions.ContainsJsonPath:[]]],'test-step-1']],com.emarte.regurgitator.core.FirstMatchBehaviour:[],null]],null]");
 	}
 }
--- src/test/java/com/emarte/regurgitator/test/ContainsXpathJsonLoaderTest.java
+++ src/test/java/com/emarte/regurgitator/test/ContainsXpathJsonLoaderTest.java
@@ -20,6 +20,6 @@ public class ContainsXpathJsonLoaderTest extends JsonLoaderTest {
 
 	@Test
 	public void testFullLoad() throws DocumentException, SAXException, IOException, RegurgitatorException {
-		assertExpectationFullLoad("classpath:/ContainsXpath_fullLoad.json", "com.emarte.regurgitator.core.Sequence:['regurgitator-configuration-1',[com.emarte.regurgitator.core.Decision:['decision-1',[com.emarte.regurgitator.test.stuff.TestStep:['test-step-1']],[com.emarte.regurgitator.core.Rule:['rule-1',[com.emarte.regurgitator.core.Condition:['condition-1',com.emarte.regurgitator.core.ContextLocation:['context:location'],'/something/something',true,com.emarte.regurgitator.extensions.ContainsXpath:[{prefix1=url1, prefix2=url2}]]],'test-step-1']],com.emarte.regurgitator.core.FirstMatchBehaviour:[],null]]]");
+		assertExpectationFullLoad("classpath:/ContainsXpath_fullLoad.json", "com.emarte.regurgitator.core.Sequence:['regurgitator-configuration-1',[com.emarte.regurgitator.core.Decision:['decision-1',[com.emarte.regurgitator.test.stuff.TestStep:['test-step-1']],[com.emarte.regurgitator.core.Rule:['rule-1',[com.emarte.regurgitator.core.Condition:['condition-1',com.emarte.regurgitator.core.ContextLocation:['context:location'],'/something/something',true,com.emarte.regurgitator.extensions.ContainsXpath:[{prefix1=url1, prefix2=url2}]]],'test-step-1']],com.emarte.regurgitator.core.FirstMatchBehaviour:[],null]],null]");
 	}
 }

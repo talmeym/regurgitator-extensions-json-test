6a76b916ae207a5227c92c938d9718d7ae40ad94
talmeym <mtalmey@gmail.com>
2017-02-15 12:14:12 +0000

    Changed to use jvm supplied xml / xpath processing

--- src/test/java/com/emarte/regurgitator/test/ContainsJsonPathBehaviourJsonLoaderTest.java
+++ src/test/java/com/emarte/regurgitator/test/ContainsJsonPathBehaviourJsonLoaderTest.java
@@ -1,11 +1,6 @@
 package com.emarte.regurgitator.test;
 
-import com.emarte.regurgitator.core.RegurgitatorException;
-import org.dom4j.DocumentException;
 import org.junit.Test;
-import org.xml.sax.SAXException;
-
-import java.io.IOException;
 
 public class ContainsJsonPathBehaviourJsonLoaderTest extends JsonLoaderTest {
 	public ContainsJsonPathBehaviourJsonLoaderTest() {
@@ -13,7 +8,7 @@ public class ContainsJsonPathBehaviourJsonLoaderTest extends JsonLoaderTest {
 	}
 
 	@Test
-	public void testFullLoad() throws DocumentException, SAXException, IOException, RegurgitatorException {
+	public void testFullLoad() throws Exception {
 		assertExpectationFullLoad("classpath:/ContainsJsonPathBehaviour_fullLoad.json", "com.emarte.regurgitator.core.Sequence:['regurgitator-configuration-1',[com.emarte.regurgitator.core.Decision:['decision-1',[com.emarte.regurgitator.test.stuff.TestStep:['test-step-1']],[com.emarte.regurgitator.core.Rule:['rule-1',[com.emarte.regurgitator.core.Condition:['condition-1',com.emarte.regurgitator.core.ContextLocation:['context:location'],'$.something.something',true,com.emarte.regurgitator.extensions.ContainsJsonPathBehaviour:[]]],'test-step-1']],com.emarte.regurgitator.core.FirstMatchBehaviour:[],null]],null]");
 	}
 }
--- src/test/java/com/emarte/regurgitator/test/ContainsXpathJsonLoaderTest.java
+++ src/test/java/com/emarte/regurgitator/test/ContainsXpathJsonLoaderTest.java
@@ -1,12 +1,7 @@
 package com.emarte.regurgitator.test;
 
-import com.emarte.regurgitator.core.RegurgitatorException;
 import com.emarte.regurgitator.extensions.ContainsXpathJsonLoader;
-import org.dom4j.DocumentException;
 import org.junit.Test;
-import org.xml.sax.SAXException;
-
-import java.io.IOException;
 
 public class ContainsXpathJsonLoaderTest extends JsonLoaderTest {
 	public ContainsXpathJsonLoaderTest() {
@@ -14,22 +9,22 @@ public class ContainsXpathJsonLoaderTest extends JsonLoaderTest {
 	}
 
 	@Test
-	public void testMinJson() throws DocumentException, SAXException, IOException, RegurgitatorException {
+	public void testMinJson() throws Exception {
 		assertExpectation("classpath:/ContainsXpath_min.json", "com.emarte.regurgitator.extensions.ContainsXpath:[{this=http://something.com}]");
 	}
 
 	@Test
-	public void testMaxJson() throws DocumentException, SAXException, IOException, RegurgitatorException {
+	public void testMaxJson() throws Exception {
 		assertExpectation("classpath:/ContainsXpath_max.json", "com.emarte.regurgitator.extensions.ContainsXpath:[{this=http://something.com}]");
 	}
 
 	@Test
-	public void testFullLoad() throws DocumentException, SAXException, IOException, RegurgitatorException {
+	public void testFullLoad() throws Exception {
 		assertExpectationFullLoad("classpath:/ContainsXpath_fullLoad.json", "com.emarte.regurgitator.core.Sequence:['regurgitator-configuration-1',[com.emarte.regurgitator.core.Decision:['decision-1',[com.emarte.regurgitator.test.stuff.TestStep:['test-step-1']],[com.emarte.regurgitator.core.Rule:['rule-1',[com.emarte.regurgitator.core.Condition:['condition-1',com.emarte.regurgitator.core.ContextLocation:['context:location'],'/something/something',true,com.emarte.regurgitator.extensions.ContainsXpath:[{prefix2=url2, prefix1=url1}]]],'test-step-1']],com.emarte.regurgitator.core.FirstMatchBehaviour:[],null]],null]");
 	}
 
 	@Test
-	public void testFullLoad_entityLookup() throws DocumentException, SAXException, IOException, RegurgitatorException {
+	public void testFullLoad_entityLookup() throws Exception {
 		assertExpectationFullLoad("classpath:/ContainsXpath_fullLoad_entityLookup.json", "com.emarte.regurgitator.core.Sequence:['regurgitator-configuration-1',[com.emarte.regurgitator.core.Decision:['decision-1',[com.emarte.regurgitator.test.stuff.TestStep:['test-step-1']],[com.emarte.regurgitator.core.Rule:['rule-1',[com.emarte.regurgitator.core.Condition:['condition-1',com.emarte.regurgitator.core.ContextLocation:['context:location'],'/something/something',true,com.emarte.regurgitator.extensions.ContainsXpath:[{}]]],'test-step-1']],com.emarte.regurgitator.core.FirstMatchBehaviour:[],null]],null]");
 	}
 }

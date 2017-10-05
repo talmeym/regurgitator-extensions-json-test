1b01a64a574d2f10b5a4ac868d4cc2373bcb6fe0
talmeym <mtalmey@gmail.com>
2017-10-05 22:42:32 +0100

    sorted indentation

--- src/test/java/com/emarte/regurgitator/test/ContainsJsonPathBehaviourJsonLoaderTest.java
+++ src/test/java/com/emarte/regurgitator/test/ContainsJsonPathBehaviourJsonLoaderTest.java
@@ -3,12 +3,12 @@ package com.emarte.regurgitator.test;
 import org.junit.Test;
 
 public class ContainsJsonPathBehaviourJsonLoaderTest extends JsonLoaderTest {
-	public ContainsJsonPathBehaviourJsonLoaderTest() {
-		super(null);
-	}
+    public ContainsJsonPathBehaviourJsonLoaderTest() {
+        super(null);
+    }
 
-	@Test
-	public void testFullLoad() throws Exception {
-		assertExpectationFullLoad("classpath:/ContainsJsonPathBehaviour_fullLoad.json", "com.emarte.regurgitator.core.Sequence:['regurgitator-configuration-1',[com.emarte.regurgitator.core.Decision:['decision-1',[com.emarte.regurgitator.test.stuff.TestStep:['test-step-1']],[com.emarte.regurgitator.core.Rule:['rule-1',[com.emarte.regurgitator.core.Condition:['condition-1',com.emarte.regurgitator.core.ContextLocation:['context:location'],'$.something.something',true,com.emarte.regurgitator.extensions.ContainsJsonPathBehaviour:[]]],'test-step-1']],com.emarte.regurgitator.core.FirstMatchBehaviour:[],null]],null]");
-	}
+    @Test
+    public void testFullLoad() throws Exception {
+        assertExpectationFullLoad("classpath:/ContainsJsonPathBehaviour_fullLoad.json", "com.emarte.regurgitator.core.Sequence:['regurgitator-configuration-1',[com.emarte.regurgitator.core.Decision:['decision-1',[com.emarte.regurgitator.test.stuff.TestStep:['test-step-1']],[com.emarte.regurgitator.core.Rule:['rule-1',[com.emarte.regurgitator.core.Condition:['condition-1',com.emarte.regurgitator.core.ContextLocation:['context:location'],'$.something.something',true,com.emarte.regurgitator.extensions.ContainsJsonPathBehaviour:[]]],'test-step-1']],com.emarte.regurgitator.core.FirstMatchBehaviour:[],null]],null]");
+    }
 }

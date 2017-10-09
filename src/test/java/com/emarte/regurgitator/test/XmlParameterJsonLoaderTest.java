8e741a68447a0bdf4482a7356af9304acc4a5520
talmeym <mtalmey@gmail.com>
2017-10-09 17:47:28 +0100

    tidied indentation

--- src/test/java/com/emarte/regurgitator/test/XmlParameterJsonLoaderTest.java
+++ src/test/java/com/emarte/regurgitator/test/XmlParameterJsonLoaderTest.java
@@ -11,27 +11,27 @@ import org.junit.Test;
 import static com.emarte.regurgitator.core.ConfigurationFile.loadFile;
 
 public class XmlParameterJsonLoaderTest extends JsonLoaderTest {
-	public XmlParameterJsonLoaderTest() {
-		super(new XmlParameterJsonLoader());
-	}
+    public XmlParameterJsonLoaderTest() {
+        super(new XmlParameterJsonLoader());
+    }
 
-	@Test
-	public void testJson() throws Exception {
-		assertExpectation("classpath:/XmlParameter_min.json", "com.emarte.regurgitator.extensions.XmlParameter:['xml-parameter-1',com.emarte.regurgitator.core.ParameterPrototype:['name',com.emarte.regurgitator.core.StringType:[],com.emarte.regurgitator.core.ConflictPolicy:REPLACE],'parameters',com.emarte.regurgitator.core.ValueSource:[com.emarte.regurgitator.core.ContextLocation:['location'],null],com.emarte.regurgitator.extensions.XpathProcessor:['xpath/xpath',{prefix2=uri2, prefix1=uri1}],null]");
-	}
+    @Test
+    public void testJson() throws Exception {
+        assertExpectation("classpath:/XmlParameter_min.json", "com.emarte.regurgitator.extensions.XmlParameter:['xml-parameter-1',com.emarte.regurgitator.core.ParameterPrototype:['name',com.emarte.regurgitator.core.StringType:[],com.emarte.regurgitator.core.ConflictPolicy:REPLACE],'parameters',com.emarte.regurgitator.core.ValueSource:[com.emarte.regurgitator.core.ContextLocation:['location'],null],com.emarte.regurgitator.extensions.XpathProcessor:['xpath/xpath',{prefix2=uri2, prefix1=uri1}],null]");
+    }
 
-	@Test
-	public void testMaximumJson() throws Exception {
-		assertExpectation("classpath:/XmlParameter_max.json", "com.emarte.regurgitator.extensions.XmlParameter:['xml-parameter-1',com.emarte.regurgitator.core.ParameterPrototype:['name',com.emarte.regurgitator.core.StringType:[],com.emarte.regurgitator.core.ConflictPolicy:REPLACE],'parameters',com.emarte.regurgitator.core.ValueSource:[com.emarte.regurgitator.core.ContextLocation:['location'],null],com.emarte.regurgitator.extensions.XpathProcessor:['xpath/xpath',{prefix2=uri2, prefix1=uri1}],com.emarte.regurgitator.test.stuff.TestValueProcessor:[]]");
-	}
+    @Test
+    public void testMaximumJson() throws Exception {
+        assertExpectation("classpath:/XmlParameter_max.json", "com.emarte.regurgitator.extensions.XmlParameter:['xml-parameter-1',com.emarte.regurgitator.core.ParameterPrototype:['name',com.emarte.regurgitator.core.StringType:[],com.emarte.regurgitator.core.ConflictPolicy:REPLACE],'parameters',com.emarte.regurgitator.core.ValueSource:[com.emarte.regurgitator.core.ContextLocation:['location'],null],com.emarte.regurgitator.extensions.XpathProcessor:['xpath/xpath',{prefix2=uri2, prefix1=uri1}],com.emarte.regurgitator.test.stuff.TestValueProcessor:[]]");
+    }
 
-	@Test
-	public void testMaximumFlatJson() throws Exception {
-		assertExpectation("classpath:/XmlParameter_maxFlat.json", "com.emarte.regurgitator.extensions.XmlParameter:['xml-parameter-1',com.emarte.regurgitator.core.ParameterPrototype:['name',com.emarte.regurgitator.core.StringType:[],com.emarte.regurgitator.core.ConflictPolicy:REPLACE],'parameters',com.emarte.regurgitator.core.ValueSource:[com.emarte.regurgitator.core.ContextLocation:['location'],null],com.emarte.regurgitator.extensions.XpathProcessor:['xpath/xpath',{prefix2=uri2, prefix1=uri1}],com.emarte.regurgitator.test.stuff.TestValueProcessor:[]]");
-	}
+    @Test
+    public void testMaximumFlatJson() throws Exception {
+        assertExpectation("classpath:/XmlParameter_maxFlat.json", "com.emarte.regurgitator.extensions.XmlParameter:['xml-parameter-1',com.emarte.regurgitator.core.ParameterPrototype:['name',com.emarte.regurgitator.core.StringType:[],com.emarte.regurgitator.core.ConflictPolicy:REPLACE],'parameters',com.emarte.regurgitator.core.ValueSource:[com.emarte.regurgitator.core.ContextLocation:['location'],null],com.emarte.regurgitator.extensions.XpathProcessor:['xpath/xpath',{prefix2=uri2, prefix1=uri1}],com.emarte.regurgitator.test.stuff.TestValueProcessor:[]]");
+    }
 
-	@Test
-	public void testFullLoad() throws RegurgitatorException {
-		loadFile("classpath:/XmlParameter_fullLoad.json");
-	}
+    @Test
+    public void testFullLoad() throws RegurgitatorException {
+        loadFile("classpath:/XmlParameter_fullLoad.json");
+    }
 }

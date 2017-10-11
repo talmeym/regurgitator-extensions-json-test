4626118943dbee35fccf13fe362dcac1291cb318
talmeym <mtalmey@gmail.com>
2017-10-11 08:23:31 +0100

    tidied imports

--- src/test/java/com/emarte/regurgitator/test/XpathProcessorJsonLoaderTest.java
+++ src/test/java/com/emarte/regurgitator/test/XpathProcessorJsonLoaderTest.java
@@ -4,7 +4,7 @@
  */
 package com.emarte.regurgitator.test;
 
-import com.emarte.regurgitator.core.*;
+import com.emarte.regurgitator.core.RegurgitatorException;
 import com.emarte.regurgitator.extensions.XpathProcessorJsonLoader;
 import org.junit.Test;
 
